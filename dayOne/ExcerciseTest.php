ae4692448427c51af0fc69a1b5aae898f75ac6f8
mangopeaches <thomasjbreese@gmail.com>
2020-12-01 23:58:37 -0500

    larger input set

--- dayOne/ExcerciseTest.php
+++ dayOne/ExcerciseTest.php
@@ -8,7 +8,11 @@ class ExerciseTest extends TestCase
     public function testSuccess()
     {
         $lines = getLines(__DIR__ . "/input.txt");
-        sort($lines);
         $this->assertEquals(report($lines, 2020), 514579);
+    }
+
+    public function testLargerInput() {
+        $lines = getLines(__DIR__ . "/largerInput.txt");
+        $this->assertTrue(report($lines, 2020) > 0);
     }
 }
